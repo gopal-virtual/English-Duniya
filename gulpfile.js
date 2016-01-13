5e7dacf869a5ba88d2e4712eca54e7e7398dd789
MrGopal <gopal@zaya.in>
2016-01-13 09:04:37 +0530

    gulp file updated, basic app build

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,76 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var sh = require('shelljs');
+// var gutil = require('gulp-util');
+// var bower = require('bower');
+var concate = require('gulp-concat');
+var plumber = require('gulp-plumber');
+var print = require('gulp-print');
+var ngAnnotate = require('gulp-ng-annotate');
+// var sass = require('gulp-sass');
+// var minifyCss = require('gulp-minify-css');
+// var rename = require('gulp-rename');
+// var sh = require('shelljs');
 
 var paths = {
   sass: ['./scss/**/*.scss']
 };
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass','watch']);
+
+gulp.task('scripts', function() {
+    gulp.src(['www/js/*.js'])
+        .pipe(plumber({
+            handleError: function(err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))
+        .pipe(print(function(filepath) {
+            return "MrGopal modified : " + filepath;
+        }))
+        .pipe(ngAnnotate())
+        .pipe(concate('mobile.app.js'))
+        // .pipe(rename({
+        //     suffix: '.min'
+        // }))
+        // .pipe(uglify())
+        .pipe(gulp.dest('www/build'))
+        // .pipe(broswerSync.stream())
+})
 
 gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .on('error', sass.logError)
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
+  // gulp.src('./scss/ionic.app.scss')
+  //   .pipe(sass())
+  //   .on('error', sass.logError)
+  //   .pipe(gulp.dest('./www/css/'))
+  //   .pipe(minifyCss({
+  //     keepSpecialComments: 0
+  //   }))
+  //   .pipe(rename({ extname: '.min.css' }))
+  //   .pipe(gulp.dest('./www/css/'))
+  //   .on('end', done);
 });
 
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
+  gulp.watch(['www/js/*.js'],['scripts']);
 });
 
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
-});
+// gulp.task('install', ['git-check'], function() {
+//   return bower.commands.install()
+//     .on('log', function(data) {
+//       gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+//     });
+// });
 
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
-});
+// gulp.task('git-check', function(done) {
+//   if (!sh.which('git')) {
+//     console.log(
+//       '  ' + gutil.colors.red('Git is not installed.'),
+//       '\n  Git, the version control system, is required to download Ionic.',
+//       '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+//       '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+//     );
+//     process.exit(1);
+//   }
+//   done();
+// });
