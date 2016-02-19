2306cf02e50714b878aa526a26dd629d9a70cb6a
MrGopal <gopal@zaya.in>
2016-02-20 02:18:34 +0530

    white screen error fix, ui-view changed to ion-nav-view

--- www/js/app.routes.js
+++ www/js/app.routes.js
@@ -1,4 +1,6 @@
 (function() {
+  'use strict';
+
   angular
     .module('zaya')
     .config(mainRoute);
@@ -8,38 +10,39 @@
     $stateProvider
       .state('login', {
         url: '/login',
-        templateUrl: '/template/login/login.view.html',
+        templateUrl: './template/login/login.view.html',
         controller : 'authController as Auth'
       })
       .state('user',{
         url :'/user',
         abstract : true,
-        template: '<ui-view/>'
+        template: '<ion-nav-view></ion-nav-view>'
       })
       .state('user.main',{
         url : '/main',
-        templateUrl : '/template/user/main.view.html',
+        abstract : true,
+        templateUrl : './template/user/main.view.html',
         controller : 'userMainController as UserMain'
       })
       .state('user.main.profile',{
         url : '/profile',
-        templateUrl : '/template/profile/profile.view.html'
+        templateUrl : './template/profile/profile.view.html'
       })
       .state('user.main.playlist',{
         url : '/playlist',
-        templateUrl : '/template/playlist/playlist.view.html'
+        templateUrl : './template/playlist/playlist.view.html'
       })
       .state('user.main.home',{
         url : '/home',
-        templateUrl : '/template/home/home.view.html'
+        templateUrl : './template/home/home.view.html'
       })
       .state('user.main.result',{
         url : '/result',
-        templateUrl : '/template/result/result.view.html'
+        templateUrl : './template/result/result.view.html'
       })
       .state('user.main.search',{
         url : '/search',
-        templateUrl : '/template/search/search.view.html'
+        templateUrl : './template/search/search.view.html'
       })
 
     $urlRouterProvider.otherwise('/login');
