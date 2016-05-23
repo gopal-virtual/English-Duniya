f2ae4ffb266d6dd19fa03088f4dc8073a352f38a
gopal-zaya <gopal@zaya.in>
2016-05-23 21:46:22 +0530

    map sound volume increased

--- www/templates/common/common.run.js
+++ www/templates/common/common.run.js
@@ -11,7 +11,7 @@
         $cordovaNativeAudio.preloadSimple('water-drop', 'sound/water-drop.mp3');
         $cordovaNativeAudio.preloadSimple('correct', 'sound/correct.mp3');
         $cordovaNativeAudio.preloadSimple('wrong', 'sound/wrong.wav');
-        $cordovaNativeAudio.preloadComplex('background', 'sound/background.mp3',0.1,1,0,function(){});
+        $cordovaNativeAudio.preloadComplex('background', 'sound/background.mp3',0.5,1,0,function(){});
         $cordovaNativeAudio.preloadSimple('one_star', 'sound/one_star.mp3');
         $cordovaNativeAudio.preloadSimple('two_star', 'sound/two_star.mp3');
         $cordovaNativeAudio.preloadSimple('three_star', 'sound/three_star.mp3');
