225e763536fc98c02f68994aa5accca29b7c718a
gopal-zaya <gopal@zaya.in>
2016-08-09 16:15:43 +0530

    mend

--- setup.sh
+++ setup.sh
@@ -17,6 +17,9 @@ cd android-sdk-linux/tools
 # export PATH=${PATH}:/home/android-sdk-linux/platform-tools:/home/android-sdk-linux/tools:/home/android-sdk-linux/build-tools/22.0.1/
 # source ~/.bashrc
 
+# android list sdk
+# sudo android update sdk -u -a -t <package number>
+
 # # adb
 sudo apt-get -y install libc6:i386 libstdc++6:i386 zlib1g:i386
 
@@ -26,5 +29,4 @@ sudo npm install -g cordova ionic gulp bower pouchdb-dump-cli pouchdb-server
 sudo npm install
 bower install
 gulp
-#
 ionic state restore
