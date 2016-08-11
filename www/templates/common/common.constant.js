7993edac0348b9bd115130ec482aefa3bf1ee586
brayn003 <brayn003@gmail.com>
2016-08-11 18:19:53 +0530

    build values

--- www/templates/common/common.constant.js
+++ www/templates/common/common.constant.js
@@ -5,12 +5,12 @@
     .module('common')
     .constant('CONSTANT', {
       'LOCK': true,
-      'BACKEND_SERVICE_DOMAIN': 'https://cc-test.zaya.in/',
+      // 'BACKEND_SERVICE_DOMAIN': 'https://cc-test.zaya.in/',
       // 'BACKEND_SERVICE_DOMAIN': 'http://cc-pre-prod.zaya.in/',
-      // 'BACKEND_SERVICE_DOMAIN': 'https://eg-api.zaya.in/',
-      'RESOURCE_SERVER': 'https://cc-test.zaya.in/',
+      'BACKEND_SERVICE_DOMAIN': 'https://eg-api.zaya.in/',
+      // 'RESOURCE_SERVER': 'https://cc-test.zaya.in/',
       // 'RESOURCE_SERVER': 'http://cc-pre-prod.zaya.in/',
-      // 'RESOURCE_SERVER': 'https://eg-api.zaya.in/',
+      'RESOURCE_SERVER': 'https://eg-api.zaya.in/',
       // 'BACKEND_SERVICE_DOMAIN' : 'http://192.168.1.6:9000/',
       'QUESTION' : {
         'DEMO' : '33c0asdf-b7f6-4f1c-8db5-234f5613a854'
