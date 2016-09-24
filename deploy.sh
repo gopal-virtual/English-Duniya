3b70dd9e6af5c22fcadbe7636289b3e644ddf207
kartikagarwal1993 <kartikagarwal.1993@gmail.com>
2016-09-24 12:58:05 +0530

    testing bundlecontent 1

--- deploy.sh
+++ deploy.sh
@@ -1,37 +1,47 @@
-node bundleContent.js 3
-gulp --env=prod
-developer
-
 #!/bin/bash
-# pouchdb-server --port 5984 &
+source /home/ubuntu/.bashrc
+export ANDROID_HOME=/home/ubuntu/apps/android-sdk-linux
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/ubuntu/apps/android-sdk-linux/platform-tools:/home/ubuntu/apps/android-sdk-linux/tools:/opt/nodej/bin
+export NODE_PATH=/opt/nodej/lib/node_modules
+cd $WORKSPACE
+npm install
+bower install
+echo "Initiating build process "
+REPO_PATH=$WORKSPACE
+cd $REPO_PATH
+cp $REPO_PATH/resources/android/drawable-xxhdpi/icon.png $REPO_PATH/resources/android/drawable-xxdpi/icon.png
+mkdir -p $REPO_PATH/resources/android/drawable-xxdpi
+cp $REPO_PATH/resources/android/drawable-xxhdpi/icon.png $REPO_PATH/resources/android/drawable-xxdpi/icon.png
+mkdir -p $REPO_PATH/resources/android/drawable-xxxdpi
+cp $REPO_PATH/resources/android/drawable-xxhdpi/icon.png  $REPO_PATH/resources/android/drawable-xxxdpi/icon.png
+mkdir -p  $REPO_PATH/resources/android/splash/
+cp $REPO_PATH/resources/android/drawable-mdpi/screen.png $REPO_PATH/resources/android/splash/screen.png
 
-# sleep 2
-# gulp
-# node port.js
-# rm $PWD/www/data/lessons*
-# pouchdb-dump http://localhost:5984/lessonsGrade1 > $PWD/www/data/lessonsGrade1.db
-# pouchdb-dump http://localhost:5984/lessonsGrade2 > $PWD/www/data/lessonsGrade2.db
-# pouchdb-dump http://localhost:5984/lessonsGrade3 > $PWD/www/data/lessonsGrade3.db
-
-rm $PWD/*.apk
+echo "Bundling Content"
+node bundleContent.js 3
+echo "starting to build"
 ionic build android
-# ionic build android --xwalk64bit
 cordova build --release android
+
 # cordova build --release android --xwalk64bit
-#jarsigner -verbose -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore classcloud.keystore -storepass zayaayaz1234 $PWD/platforms/android/build/outputs/apk/android-x86-release-unsigned.apk angryape
+#jarsigner -verbose -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore classcloud.keystore -storepass zayaayaz1234 $REPO_PATH/platforms/android/build/outputs/apk/android-x86-release-unsigned.apk angryape
 # jarsigner -verbose -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore classcloud.keystore -storepass zayaayaz1234 $PWD/platforms/android/build/outputs/apk/android-x86_64-release-unsigned.apk angryape
-jarsigner -verbose -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore classcloud.keystore -storepass zayaayaz1234 $PWD/platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk angryape
+#jarsigner -verbose -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore classcloud.keystore -storepass zayaayaz1234 $REPO_PATH/platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk angryape
 # jarsigner -verbose -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore classcloud.keystore -storepass zayaayaz1234 $PWD/platforms/android/build/outputs/apk/android-arm64-release-unsigned.apk angryape
 # jarsigner -verbose -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore classcloud.keystore -storepass zayaayaz1234 $PWD/platforms/android/build/outputs/apk/android-release-unsigned.apk angryape
-select VERSION in $ANDROID_HOME/build-tools/*;
-do
-#  $VERSION/zipalign -v 4 $PWD/platforms/android/build/outputs/apk/android-x86-release-unsigned.apk $PWD/angryape_x86.apk
+#select VERSION in $ANDROID_HOME/build-tools/*;
+VERSION="$ANDROID_HOME/build-tools/23.0.1"
+BUILD_PATH="/tmp"
+BUILD_NAME="englishduniya-$BUILD_NUMBER"
+echo $BUILD_NAME
+#do
+#  $VERSION/zipalign -v 4 $REPO_PATH/platforms/android/build/outputs/apk/android-x86-release-unsigned.apk "$BUILD_PATH/$BUILD_NAME-x86.apk"
   # $VERSION/zipalign -v 4 $PWD/platforms/android/build/outputs/apk/android-x86_64-release-unsigned.apk $PWD/angryape_x86_64.apk
- $VERSION/zipalign -v 4 $PWD/platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk $PWD/angryape_armv7.apk
+#  $VERSION/zipalign -v 4 $REPO_PATH/platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk "$BUILD_PATH/$BUILD_NAME-armv7.apk"
   # $VERSION/zipalign -v 4 $PWD/platforms/android/build/outputs/apk/android-arm64-release-unsigned.apk $PWD/angryape_armv64.apk
   # $VERSION/zipalign -v 4 $PWD/platforms/android/build/outputs/apk/android-release-unsigned.apk $PWD/angryape.apk
-  break
-done
+  #break
+#done
 kill -9 `ps aux | grep pouchdb-server | grep -v grep | awk '{print $2}'`
 cat << "EOF"
   /$$$$$$                                                 /$$$$$$
@@ -46,3 +56,6 @@ cat << "EOF"
                     |  $$$$$$/          |  $$$$$$/                | $$
                      \______/            \______/                 |__/
 EOF
+
+
+
