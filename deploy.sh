009bbb84bfaed1f335292d85cfe5b4409891d0bc
kartikagarwal1993 <kartikagarwal.1993@gmail.com>
2016-09-26 17:25:03 +0530

    Testing builds ci

--- deploy.sh
+++ deploy.sh
@@ -14,8 +14,8 @@ USERNAME='admin'
 PASSWORD='admin-builds'
 cd $REPO_PATH
 
-#npm install
-#bower install
+npm install
+bower install
 
 echo "Starting content sync"
 rsync -avzh ubuntu@eg-api.zaya.in:/home/ubuntu/classcloud/classcloud/media/ell media/
@@ -50,12 +50,12 @@ echo "Configuring Environment for "$ENV
 gulp --env=$ENV
 
 echo "starting to build"
-#ionic build android
-#cordova build --release android
+ionic build android
+cordova build --release android
 # cordova build --release android --xwalk64bit
-#jarsigner -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore classcloud.keystore -storepass zayaayaz1234 $REPO_PATH/platforms/android/build/outputs/apk/android-x86-release-unsigned.apk angryape
+jarsigner -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore classcloud.keystore -storepass zayaayaz1234 $REPO_PATH/platforms/android/build/outputs/apk/android-x86-release-unsigned.apk angryape
 # jarsigner -verbose -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore classcloud.keystore -storepass zayaayaz1234 $PWD/platforms/android/build/outputs/apk/android-x86_64-release-unsigned.apk angryape
-#jarsigner -verbose -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore classcloud.keystore -storepass zayaayaz1234 $REPO_PATH/platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk angryape
+jarsigner -verbose -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore classcloud.keystore -storepass zayaayaz1234 $REPO_PATH/platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk angryape
 # jarsigner -verbose -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore classcloud.keystore -storepass zayaayaz1234 $PWD/platforms/android/build/outputs/apk/android-arm64-release-unsigned.apk angryape
 # jarsigner -verbose -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore classcloud.keystore -storepass zayaayaz1234 $PWD/platforms/android/build/outputs/apk/android-release-unsigned.apk angryape
 VERSION="$ANDROID_HOME/build-tools/23.0.1"
@@ -70,26 +70,37 @@ if [ "$i" == "all" ]; then
 BUILD_NAME="englishduniya-$ENV-bundled-$BUILD_NUMBER"
 BUNDLED=" Bundled "
 fi
-#echo $BUILD_NAME
+echo $BUILD_NAME
 X86_BUILD_NAME="$BUILD_PATH/$BUILD_NAME-x86.apk"
 ARM_BUILD_NAME="$BUILD_PATH/$BUILD_NAME-arm.apk"
 echo $X86_BUILD_NAME
 #select VERSION in $ANDROID_HOME/build-tools/*;
 #do
 echo "$BUILD_PATH/$BUILD_NAME-x86.apk"
-#  $VERSION/zipalign -v 4 $REPO_PATH/platforms/android/build/outputs/apk/android-x86-release-unsigned.apk $X86_BUILD_NAME
+  $VERSION/zipalign -v 4 $REPO_PATH/platforms/android/build/outputs/apk/android-x86-release-unsigned.apk $X86_BUILD_NAME
   # $VERSION/zipalign -v 4 $PWD/platforms/android/build/outputs/apk/android-x86_64-release-unsigned.apk $PWD/angryape_x86_64.apk
-  # $VERSION/zipalign -v 4 $REPO_PATH/platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk "$BUILD_PATH/$BUILD_NAME-armv7.apk"
+  $VERSION/zipalign -v 4 $REPO_PATH/platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk $ARM_BUILD_NAME
   # $VERSION/zipalign -v 4 $PWD/platforms/android/build/outputs/apk/android-arm64-release-unsigned.apk $PWD/angryape_armv64.apk
   # $VERSION/zipalign -v 4 $PWD/platforms/android/build/outputs/apk/android-release-unsigned.apk $PWD/angryape.apk
 #  break
 #done
 
 
-echo "club -h $HOST -t $BUILD_TYPE -l $BUILD_PLATFORM -a x86 -f /tmp/englishduniya-dev-non-bundled-67-x86.apk -u $USERNAME -p $PASSWORD -d $BUNDLED $BUILD_DESCRIPTION"
-/usr/local/bin/club -h $HOST -t $BUILD_TYPE -l $BUILD_PLATFORM -a x86 -f /tmp/englishduniya-dev-non-bundled-67-x86.apk -u $USERNAME -p $PASSWORD -d "$BUNDLED $BUILD_DESCRIPTION"
+#echo "club -h $HOST -t $BUILD_TYPE -l $BUILD_PLATFORM -a x86 -f $X86_BUILD_NAME -u $USERNAME -p $PASSWORD -d $BUNDLED $BUILD_DESCRIPTION"
+/usr/local/bin/club -h $HOST -t $BUILD_TYPE -l $BUILD_PLATFORM -a x86 -f $X86_BUILD_NAME -u $USERNAME -p $PASSWORD -d "$BUNDLED $BUILD_DESCRIPTION"
+/usr/local/bin/club -h $HOST -t $BUILD_TYPE -l $BUILD_PLATFORM -a arm -f $ARM_BUILD_NAME -u $USERNAME -p $PASSWORD -d "$BUNDLED $BUILD_DESCRIPTION"
 done
 
+
+if [ $? -eq 0 ] ; then
+	echo "******************************** Starting to create tag and push ************************************"
+    TAG_NAME="$BUILD_TYPE-#$BUILD_NUMBER"
+    echo $TAG_NAME
+    TAG_MESSAGE="Tag created on branch $BRANCH_NAME for commit $GIT_COMMIT by jenkins job $BUILD_NUMBER. [Message] $COMMIT_MESSAGE "
+	echo $TAG_MESSAGE
+    git tag -fa "$TAG_NAME" -m "$TAG_MESSAGE"
+    ssh-agent bash -lc "ssh-add $HOME/.ssh/zaya_mobile  && git push origin --tags "
+fi
 
 #kill -9 `ps aux | grep pouchdb-server | grep -v grep | awk '{print $2}'`
 cat << "EOF"
