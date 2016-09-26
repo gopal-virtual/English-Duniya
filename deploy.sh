09d653d2896e254ddcacab35abfddbfecc089f63
kartikagarwal1993 <kartikagarwal.1993@gmail.com>
2016-09-26 14:52:38 +0530

    testing club

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ export ANDROID_HOME=/home/ubuntu/apps/android-sdk-linux
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/ubuntu/apps/android-sdk-linux/platform-tools:/home/ubuntu/apps/android-sdk-linux/tools:/opt/nodej/bin
 export NODE_PATH=/opt/nodej/lib/node_modules
 REPO_PATH=$WORKSPACE
-HOST="https://builds.zaya.in/"
+HOST="builds.zaya.in/"
 BUILD_PLATFORM='android'
 echo "Git variables values are "
 BRANCH_NAME=`echo $GIT_BRANCH |grep -o "/.*"|grep -o "[A-Za-z0-9].*"`
@@ -50,8 +50,8 @@ echo "Configuring Environment for "$ENV
 gulp --env=$ENV
 
 echo "starting to build"
-#ionic build android
-#cordova build --release android
+ionic build android
+cordova build --release android
 # cordova build --release android --xwalk64bit
 jarsigner -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore classcloud.keystore -storepass zayaayaz1234 $REPO_PATH/platforms/android/build/outputs/apk/android-x86-release-unsigned.apk angryape
 # jarsigner -verbose -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore classcloud.keystore -storepass zayaayaz1234 $PWD/platforms/android/build/outputs/apk/android-x86_64-release-unsigned.apk angryape
@@ -75,7 +75,7 @@ echo $X86_BUILD_NAME
 #select VERSION in $ANDROID_HOME/build-tools/*;
 #do
 echo "$BUILD_PATH/$BUILD_NAME-x86.apk"
-#  $VERSION/zipalign -v 4 $REPO_PATH/platforms/android/build/outputs/apk/android-x86-release-unsigned.apk $X86_BUILD_NAME
+  $VERSION/zipalign -v 4 $REPO_PATH/platforms/android/build/outputs/apk/android-x86-release-unsigned.apk $X86_BUILD_NAME
   # $VERSION/zipalign -v 4 $PWD/platforms/android/build/outputs/apk/android-x86_64-release-unsigned.apk $PWD/angryape_x86_64.apk
   # $VERSION/zipalign -v 4 $REPO_PATH/platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk "$BUILD_PATH/$BUILD_NAME-armv7.apk"
   # $VERSION/zipalign -v 4 $PWD/platforms/android/build/outputs/apk/android-arm64-release-unsigned.apk $PWD/angryape_armv64.apk
@@ -85,9 +85,8 @@ echo "$BUILD_PATH/$BUILD_NAME-x86.apk"
 
 
 #echo "Club Vars "$BRANCH"-"$BUILD_DESCRIPTION"-"$HOST"-"$BUILD_DESCRIPTION"-"$BUILD_PATH
-echo "club -h $HOST -t $BUILD_TYPE -l $BUILD_PLATFORM -a arm -d $BUILD_DESCRIPTION -f $ARM_BUILD_NAME -u $USERNAME -p $PASSWORD"
-
-club -h $HOST -t $BUILD_TYPE -l $BUILD_PLATFORM -a arm -d $BUILD_DESCRIPTION -f $ARM_BUILD_NAME -u $USERNAME -p $PASSWORD
+echo "club -h $HOST -t $BUILD_TYPE -l $BUILD_PLATFORM -a arm -d \"$BUILD_DESCRIPTION\" -f $ARM_BUILD_NAME -u $USERNAME -p $PASSWORD"
+club -h $HOST -t $BUILD_TYPE -l $BUILD_PLATFORM -a arm -d \"$BUILD_DESCRIPTION\" -f $ARM_BUILD_NAME -u $USERNAME -p $PASSWORD
 done
 
 
