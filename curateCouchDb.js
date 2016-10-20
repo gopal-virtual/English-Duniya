f68b2d799f97302094797c7734c4cf17a791572a
kartikagarwal1993 <kartikagarwal.1993@gmail.com>
2016-10-20 18:31:46 +0530

    using ci-curate as couch db server

--- curateCouchDb.js
+++ curateCouchDb.js
@@ -6,6 +6,8 @@ var fs = require('fs');
 var request = require('request');
 var json = [];
 
+var couch_server = 'http://zaya-couch:zayaayaz1234@ci-couch.zaya.in/lessonsdb/'
+
 var i;
 fs.readFile('lesson.json', 'utf8', function (err, data) {
   if (err) {
@@ -14,14 +16,14 @@ fs.readFile('lesson.json', 'utf8', function (err, data) {
   json = JSON.parse(data);
 
   request({
-    url: 'http://127.0.0.1:5984/lessons/',
+    url: couch_server,
     method: 'DELETE'
   }, function (error, response, body) {
 
     if (!error) {
 
       request({
-        url: 'http://127.0.0.1:5984/lessons/',
+        url: couch_server,
         method: 'PUT'
       }, function (error, response, body) {
         if (!error) {
@@ -29,7 +31,7 @@ fs.readFile('lesson.json', 'utf8', function (err, data) {
             json[i].scores = {};
             json[i].key = i;
             request({
-              url: 'http://127.0.0.1:5984/lessons/' + json[i].node.id, //URL to hit
+              url: couch_server+ json[i].node.id, //URL to hit
               method: 'PUT',
               //Lets post the following key/values as form
               json: {lesson: json[i]}
