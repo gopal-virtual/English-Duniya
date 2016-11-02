3c6f976e8c4993c180e229857a29e21b61637987
gopal-zaya <gopal@zaya.in>
2016-11-02 11:15:39 +0530

    moved js files out of www folder

--- deploy-local.sh
+++ deploy-local.sh
@@ -4,7 +4,7 @@
 #rsync -avzh ubuntu@cc-test.zaya.in:/home/ubuntu/prod_content/ell media/
 #rm www/bundled/*
 
-export ANDROID_HOME=/opt/android-sdk-linux/
+# export ANDROID_HOME=/opt/android-sdk-linux/
 
 
 echo $ANDROID_HOME
@@ -25,4 +25,13 @@ BUILD_PATH="builds"
 
 ARM_BUILD_NAME="$BUILD_PATH/$BUILD_NAME-arm.apk"
 rm $ARM_BUILD_NAME
-$ANDROID_HOME/build-tools/24.0.2/zipalign -v 4 platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk $ARM_BUILD_NAME
+select VERSION in $ANDROID_HOME/build-tools/*;
+do
+echo "$BUILD_PATH/$BUILD_NAME-x86.apk"
+  $VERSION/zipalign -v 4 platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk $ARM_BUILD_NAME
+  # $VERSION/zipalign -v 4 $PWD/platforms/android/build/outputs/apk/android-x86_64-release-unsigned.apk $PWD/angryape_x86_64.apk
+  # $VERSION/zipalign -v 4 $REPO_PATH/platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk $ARM_BUILD_NAME
+  # $VERSION/zipalign -v 4 $PWD/platforms/android/build/outputs/apk/android-arm64-release-unsigned.apk $PWD/angryape_armv64.apk
+  # $VERSION/zipalign -v 4 $PWD/platforms/android/build/outputs/apk/android-release-unsigned.apk $PWD/angryape.apk
+ break
+done
