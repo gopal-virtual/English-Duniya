7067b90f1b7c90b0b58c752a8f4f68aea3dbb00f
kartikagarwal1993 <kartikagarwal.1993@gmail.com>
2016-11-22 12:39:39 +0530

    checking for design docs in lesson rendering

--- app_modules/common/common.content.factory.js
+++ app_modules/common/common.content.factory.js
@@ -327,8 +327,8 @@
         var currentLesson = {}
         for (var i = 0; i < data.rows.length; i++) {
         //   data.rows[i].doc.lesson.node.key = data.rows[i].doc.lesson.key;
-          $log.debug("====",data.rows[i])
-            if(data.rows[i].doc && data.rows[i]._id){
+          $log.debug("====",data.rows[i].id)
+            if(data.rows[i].doc && data.rows[i].id !== '_design/_auth'){
                 currentLesson = data.rows[i].doc.lesson
                 $log.debug('modified lesson', data.rows[i])
                 for (var c = 0; c < currentLesson.objects.length; c++) {
