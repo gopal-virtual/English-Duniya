9a4b3744485d82c717e8b7e4d663cce82a1fefce
kartikagarwal1993 <kartikagarwal.1993@gmail.com>
2016-11-25 17:39:19 +0530

    installing fs-extra to ensure if directory is already made by bundlecontent js

--- bundleContent.js
+++ bundleContent.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs = require('fs');
+var fs_extra = require('fs-extra');
 // var request = require('request');
 // var wget = require('wget-improved');
 var source_folder = 'media/ell/';
@@ -38,55 +39,55 @@ fs.readFile('lesson.json', 'utf8', function(err, data) {
   // console.log(json)
 
   fs.readFile('diagnosisQJSON.json', 'utf8', function(err, data) {
-   var diagnosis_json = JSON.parse(data);
-  var counter = [0, 0, 0, 0]
-  // Iterate lessons json
-  for (var i = 0; i < json.length; i++) {
-    // Check if lesson is to be bundled
-    if (lessons == 'all' || counter[json[i].node.type.grade] < lessons) {
-      // Add Intros to media[]
-      if (json[i].node.meta && json[i].node.meta.intros && json[i].node.meta.intros.sound) {
-        media = media.concat(json[i].node.meta.intros.sound);
-      }
-      //Iterate Resources
-      for (var j = 0; j < json[i].objects.length; j++) {
-        //if Video
-        if (json[i].objects[j].node.content_type_name == 'resource') {
-          // add video to media[]
-          media.push(json[i].objects[j].node.type.path)
+    var diagnosis_json = JSON.parse(data);
+    var counter = [0, 0, 0, 0]
+    // Iterate lessons json
+    for (var i = 0; i < json.length; i++) {
+      // Check if lesson is to be bundled
+      if (lessons == 'all' || counter[json[i].node.type.grade] < lessons) {
+        // Add Intros to media[]
+        if (json[i].node.meta && json[i].node.meta.intros && json[i].node.meta.intros.sound) {
+          media = media.concat(json[i].node.meta.intros.sound);
         }
-        // If assessment
-        if (json[i].objects[j].node.content_type_name == 'assessment') {
-          //Iterate questions
-          for (var k = 0; k < json[i].objects[j].objects.length; k++) {
-            // add widgets to media[]
-            // console.log(json[i].objects[j].objects[k].node.type.content.widgets);
-            if (json[i].objects[j].objects[k].node.type.content && json[i].objects[j].objects[k].node.type.content.widgets) {
-              for (var mediaType in json[i].objects[j].objects[k].node.type.content.widgets) {
-                if (json[i].objects[j].objects[k].node.type.content.widgets.hasOwnProperty(mediaType)) {
-                  for (var file in json[i].objects[j].objects[k].node.type.content.widgets[mediaType]) {
-                    media.push(json[i].objects[j].objects[k].node.type.content.widgets[mediaType][file]);
+        //Iterate Resources
+        for (var j = 0; j < json[i].objects.length; j++) {
+          //if Video
+          if (json[i].objects[j].node.content_type_name == 'resource') {
+            // add video to media[]
+            media.push(json[i].objects[j].node.type.path)
+          }
+          // If assessment
+          if (json[i].objects[j].node.content_type_name == 'assessment') {
+            //Iterate questions
+            for (var k = 0; k < json[i].objects[j].objects.length; k++) {
+              // add widgets to media[]
+              // console.log(json[i].objects[j].objects[k].node.type.content.widgets);
+              if (json[i].objects[j].objects[k].node.type.content && json[i].objects[j].objects[k].node.type.content.widgets) {
+                for (var mediaType in json[i].objects[j].objects[k].node.type.content.widgets) {
+                  if (json[i].objects[j].objects[k].node.type.content.widgets.hasOwnProperty(mediaType)) {
+                    for (var file in json[i].objects[j].objects[k].node.type.content.widgets[mediaType]) {
+                      media.push(json[i].objects[j].objects[k].node.type.content.widgets[mediaType][file]);
+                    }
                   }
                 }
               }
-            }
-            //add instructions to media[]
-            if (json[i].objects[j].objects[k].node.meta && json[i].objects[j].objects[k].node.meta.instructions && json[i].objects[j].objects[k].node.meta.instructions.sounds) {
-              // console.log("a",json[i].objects[j].objects[k].node.meta.instructions.sounds, typeof json[i].objects[j].objects[k].node.meta.instructions.sounds,media)
-              media = media.concat(json[i].objects[j].objects[k].node.meta.instructions.sounds);
-              console.log()
-              // console.log("a",json[i].objects[j].objects[k].node.meta.instructions.sounds, typeof json[i].objects[j].objects[k].node.meta.instructions.sounds,media)
+              //add instructions to media[]
+              if (json[i].objects[j].objects[k].node.meta && json[i].objects[j].objects[k].node.meta.instructions && json[i].objects[j].objects[k].node.meta.instructions.sounds) {
+                // console.log("a",json[i].objects[j].objects[k].node.meta.instructions.sounds, typeof json[i].objects[j].objects[k].node.meta.instructions.sounds,media)
+                media = media.concat(json[i].objects[j].objects[k].node.meta.instructions.sounds);
+                console.log()
+                // console.log("a",json[i].objects[j].objects[k].node.meta.instructions.sounds, typeof json[i].objects[j].objects[k].node.meta.instructions.sounds,media)
 
+              }
             }
+
+
           }
-
-
         }
+        // Increase Counter
+        counter[json[i].node.type.grade]++;
       }
-      // Increase Counter
-      counter[json[i].node.type.grade]++;
     }
-  }
 
     //Iterate diagnsosis questions json
     console.log("Diagnosis");
@@ -94,69 +95,72 @@ fs.readFile('lesson.json', 'utf8', function(err, data) {
     for(var prop in diagnosis_json[0]){
       // console.log(prop,diagnosis_json[0][prop])
       for(var media_type in diagnosis_json[0][prop].node.type.content.widgets){
-          if(diagnosis_json[0][prop].node.type.content.widgets.hasOwnProperty(media_type)){
+        if(diagnosis_json[0][prop].node.type.content.widgets.hasOwnProperty(media_type)){
 
-            for(var media_file in diagnosis_json[0][prop].node.type.content.widgets[media_type]){
-              console.log(diagnosis_json[0][prop].node.type.content.widgets[media_type][media_file])
-              media.push(diagnosis_json[0][prop].node.type.content.widgets[media_type][media_file]);
-            }
+          for(var media_file in diagnosis_json[0][prop].node.type.content.widgets[media_type]){
+            console.log(diagnosis_json[0][prop].node.type.content.widgets[media_type][media_file])
+            media.push(diagnosis_json[0][prop].node.type.content.widgets[media_type][media_file]);
           }
         }
-
-    }
-
-  var media_deleted = 0;
-  var media_retained = 0;
-  var strip_media = [];
-  media = media.reduce(function(a,b){
-    if (a.indexOf(b) < 0 ) a.push(b);
-    return a;
-  },[]);
-  console.log("Found " + media.length + " media files");
-  console.log(media)
-  for (i in media) {
-    strip_media[i] = getFileNameFromURl(media[i]);
-  }
-  for (i in dir_contents) {
-
-    // console.log(dir_contents[i])
-    // console.log(strip_media.indexOf(dir_contents[i]));
-    if (strip_media.indexOf(dir_contents[i]) < 0) {
-      media_deleted++;
-      // console.log("Deleted",dir_contents[i])
-      fs.unlinkSync(target_folder + dir_contents[i])
-    } else {
-      // console.log("Retained",dir_contents[i])
-      media_retained++;
-      // if(dir_contents[i] === '002.mp3' || dir_contents[i] ===  '001.mp3'){
-      //   console.log("we have found",dir_contents[i],strip_media.indexOf(dir_contents[i]),media.length)
-      // }
-      media.splice(strip_media.indexOf(dir_contents[i]), 1);
-      strip_media.splice(strip_media.indexOf(dir_contents[i]), 1);
-      // if(dir_contents[i] === '002.mp3' || dir_contents[i] ===  '001.mp3'){
-      //   console.log(media.length)
-      //   console.log("we have found",dir_contents[i],strip_media.indexOf(dir_contents[i]),media.length)
-      // }
-    }
-    if(strip_media.indexOf(dir_contents[i]) >= 0){
-      console.log("Found again")
-      i--;
-    }
-  }
-  // console.log(media)
-  console.log("Retained " + media_retained + " media files");
-  console.log("Deleted " + media_deleted + " media files");
-  console.log("Please wait while we bundle " + media.length + " media files");
-
-
-  for (i in media) {
-    var filename = getFileNameFromURl(media[i]);
-    ncp(source_folder + media[i].split('/')[media[i].split('/').length-2] +'/'+  media[i].split('/').pop(), target_folder+filename,function(error){
-      if(error){
-        console.log("Error Occured",error)
       }
-    });
-  }
 
-});
+    }
+
+    var media_deleted = 0;
+    var media_retained = 0;
+    var strip_media = [];
+    media = media.reduce(function(a,b){
+      if (a.indexOf(b) < 0 ) a.push(b);
+      return a;
+    },[]);
+    console.log("Found " + media.length + " media files");
+    console.log(media)
+    for (i in media) {
+      strip_media[i] = getFileNameFromURl(media[i]);
+    }
+    for (i in dir_contents) {
+
+      // console.log(dir_contents[i])
+      // console.log(strip_media.indexOf(dir_contents[i]));
+      if (strip_media.indexOf(dir_contents[i]) < 0) {
+        media_deleted++;
+        // console.log("Deleted",dir_contents[i])
+        fs.unlinkSync(target_folder + dir_contents[i])
+      } else {
+        // console.log("Retained",dir_contents[i])
+        media_retained++;
+        // if(dir_contents[i] === '002.mp3' || dir_contents[i] ===  '001.mp3'){
+        //   console.log("we have found",dir_contents[i],strip_media.indexOf(dir_contents[i]),media.length)
+        // }
+        media.splice(strip_media.indexOf(dir_contents[i]), 1);
+        strip_media.splice(strip_media.indexOf(dir_contents[i]), 1);
+        // if(dir_contents[i] === '002.mp3' || dir_contents[i] ===  '001.mp3'){
+        //   console.log(media.length)
+        //   console.log("we have found",dir_contents[i],strip_media.indexOf(dir_contents[i]),media.length)
+        // }
+      }
+      if(strip_media.indexOf(dir_contents[i]) >= 0){
+        console.log("Found again")
+        i--;
+      }
+    }
+    // console.log(media)
+    console.log("Retained " + media_retained + " media files");
+    console.log("Deleted " + media_deleted + " media files");
+    console.log("Please wait while we bundle " + media.length + " media files");
+
+
+    for (i in media) {
+      var filename = getFileNameFromURl(media[i]);
+      console.log("Ensuring",target_folder+filename)
+      fs_extra.ensureFileSync(target_folder+filename);
+
+      ncp(source_folder + media[i].split('/')[media[i].split('/').length-2] +'/'+  media[i].split('/').pop(), target_folder+filename,function(error){
+        if(error){
+          console.log("Error Occured");
+        }
+      });
+    }
+
+  });
 });
