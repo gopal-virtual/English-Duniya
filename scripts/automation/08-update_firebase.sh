8eb166933890bac9470fe1fc8bded6c436bb2ffe
kartikagarwal1993 <kartikagarwal.1993@gmail.com>
2017-02-01 16:48:40 +0530

    adding -y in forebase command

--- scripts/automation/08-update_firebase.sh
+++ scripts/automation/08-update_firebase.sh
@@ -1,3 +1,3 @@
-echo Update firebase --project fos-app-ed2c2 database:update /$campaign_owner_name/campaigns/$campaign_name/ scripts/status.json  --token 1/0fNl3uwDYBPhXJMHL9Oa-WLjS4lZxMYs5urCJdKKjm0
-firebase --project fos-app-ed2c2 database:update /$campaign_owner_name/campaigns/$campaign_name/ scripts/status.json  --token 1/0fNl3uwDYBPhXJMHL9Oa-WLjS4lZxMYs5urCJdKKjm0
-echo Done update firenase
+echo Update Firebase --project fos-app-ed2c2 database:update /$campaign_owner_name/campaigns/$campaign_name/ scripts/status.json  --token 1/0fNl3uwDYBPhXJMHL9Oa-WLjS4lZxMYs5urCJdKKjm0 -y
+firebase --project fos-app-ed2c2 database:update /$campaign_owner_name/campaigns/$campaign_name/ scripts/status.json  --token 1/0fNl3uwDYBPhXJMHL9Oa-WLjS4lZxMYs5urCJdKKjm0 -y
+echo Done update Firebase
