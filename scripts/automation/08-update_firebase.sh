6bf497bc6fc1bcc4a9d30ee51178903dd0cb7bd5
kartikagarwal1993 <kartikagarwal.1993@gmail.com>
2017-02-01 16:34:23 +0530

    adding project name in firebase update script

--- scripts/automation/08-update_firebase.sh
+++ scripts/automation/08-update_firebase.sh
@@ -1,1 +1,3 @@
-firebase database:update /$campaign_owner_name/campaigns/$campaign_name/ scripts/status.json  --token 1/0fNl3uwDYBPhXJMHL9Oa-WLjS4lZxMYs5urCJdKKjm0
+echo Update firebase --project fos-app-ed2c2 database:update /$campaign_owner_name/campaigns/$campaign_name/ scripts/status.json  --token 1/0fNl3uwDYBPhXJMHL9Oa-WLjS4lZxMYs5urCJdKKjm0
+firebase --project fos-app-ed2c2 database:update /$campaign_owner_name/campaigns/$campaign_name/ scripts/status.json  --token 1/0fNl3uwDYBPhXJMHL9Oa-WLjS4lZxMYs5urCJdKKjm0
+echo Done update firenase
