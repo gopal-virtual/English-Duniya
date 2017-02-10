ddb6f06d52a699fa12910814592b419e4410cbbd
kartikagarwal1993 <kartikagarwal.1993@gmail.com>
2017-02-10 18:39:40 +0530

    add userjoined state in map controller

--- app_modules/map/map.controller.js
+++ app_modules/map/map.controller.js
@@ -163,6 +163,7 @@
     mapCtrl.syncPointsQueue2 = syncPointsQueue2;
     mapCtrl.openChallenge = openChallenge;
     mapCtrl.isUserEligibleForChallenge = challenge.isUserEligible();
+    mapCtrl.hasJoinedChallenge = User.hasJoinedChallenge()
     $scope.exitChooseProfile = exitChooseProfile;
     $scope.onProfileCardClick = onProfileCardClick;
     $scope.isOnline = network.isOnline();
