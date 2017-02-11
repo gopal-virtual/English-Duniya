aaa1744078952ce595b90589358d299a4f2e8d1e
Rudraprasad Das <brayn003@gmail.com>
2017-02-11 15:17:36 +0530

    testing build script

--- scripts/automation/zaya-mobile-custom-builds.sh
+++ scripts/automation/zaya-mobile-custom-builds.sh
@@ -1,10 +1,10 @@
-cat scripts/automation/01-common_setup.sh 		> scripts/automation/zaya-mobile-custom-builds-generated-script.sh 
-cat scripts/automation/02-media_sync.sh 		>> scripts/automation/zaya-mobile-custom-builds-generated-script.sh
-cat scripts/automation/03-bundle_content.sh 	>> scripts/automation/zaya-mobile-custom-builds-generated-script.sh
+# cat scripts/automation/01-common_setup.sh 		> scripts/automation/zaya-mobile-custom-builds-generated-script.sh 
+# cat scripts/automation/02-media_sync.sh 		>> scripts/automation/zaya-mobile-custom-builds-generated-script.sh
+# cat scripts/automation/03-bundle_content.sh 	>> scripts/automation/zaya-mobile-custom-builds-generated-script.sh
 cat scripts/automation/04-run_gulp.sh		 	>> scripts/automation/zaya-mobile-custom-builds-generated-script.sh
 cat scripts/automation/05-ionic_setup.sh 		>> scripts/automation/zaya-mobile-custom-builds-generated-script.sh
 cat scripts/automation/06-make_debug_build.sh 	>> scripts/automation/zaya-mobile-custom-builds-generated-script.sh 
-cat scripts/automation/07-make_release_build.sh >> scripts/automation/zaya-mobile-custom-builds-generated-script.sh
-cat scripts/automation/08-upload_build.sh 		>> scripts/automation/zaya-mobile-custom-builds-generated-script.sh
+# cat scripts/automation/07-make_release_build.sh >> scripts/automation/zaya-mobile-custom-builds-generated-script.sh
+# cat scripts/automation/08-upload_build.sh 		>> scripts/automation/zaya-mobile-custom-builds-generated-script.sh
 
 bash scripts/automation/zaya-mobile-custom-builds-generated-script.sh
