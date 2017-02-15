1c8f1777e559872cd9727651377e3d081a459a72
kartikagarwal1993 <kartikagarwal.1993@gmail.com>
2017-02-15 11:58:06 +0530

    Issue #725 - return model only when window.device exists

--- app_modules/common/common.factory.device.js
+++ app_modules/common/common.factory.device.js
@@ -19,13 +19,13 @@
 
     var devicePropeties = {
       "uuid": CONSTANT.FAKE_LOGIN ?  fake_device_uuid : ionic.Platform.device().uuid,
-      "model": CONSTANT.FAKE_DEVICE ? 'fake_model' : window.device.model,
-      "platform": CONSTANT.FAKE_DEVICE ? 'fake_platform' : window.device.platform,
-      "version": CONSTANT.FAKE_DEVICE ? 'fake_version' : window.device.version,
-      "serial": CONSTANT.FAKE_DEVICE ? 'fake_serial' : window.device.serial,
-      "manufacturer": CONSTANT.FAKE_DEVICE ? 'fake_manufacturer' : window.device.manufacturer
+      "model": CONSTANT.FAKE_DEVICE ? 'fake_model' :  window && window.device ? window.device.model : 'na',
+      "platform": CONSTANT.FAKE_DEVICE ? 'fake_platform' : window && window.device ? window.device.platform : 'na',
+      "version": CONSTANT.FAKE_DEVICE ? 'fake_version' : window && window.device ? window.device.version : 'na',
+      "serial": CONSTANT.FAKE_DEVICE ? 'fake_serial' : window && window.device ? window.device.serial : 'na',
+      "manufacturer": CONSTANT.FAKE_DEVICE ? 'fake_manufacturer' : window && window.device ? window.device.manufacturer : 'na'
     };
-    $log.debug("THIS IS THE DEVICE ID",devicePropeties.uuid);
+    $log.debug("THIS IS THE DEVICE Details"+devicePropeties);
     return devicePropeties;
 
   }
