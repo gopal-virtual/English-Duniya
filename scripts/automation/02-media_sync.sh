596e82dff7cc54b11564288fa2e98ccab8718cb1
kartikagarwal1993 <kartikagarwal.1993@gmail.com>
2017-02-18 14:48:27 +0530

    not verifyng md5 while syncing media

--- scripts/automation/02-media_sync.sh
+++ scripts/automation/02-media_sync.sh
@@ -2,11 +2,11 @@
 
 if [ "$content_environment" = 'test' ]; then
 echo  $MEDIA_SOURCE --download --remoteresource .	
-# blobxfer --saskey "?sv=2015-04-05&ss=b&srt=sco&sp=rwdlac&se=2017-02-28T15:12:28Z&st=2016-11-10T07:12:28Z&spr=https&sig=RGOG%2Bj0fU%2B4q1Puwo%2B8WXq5p3%2BzDLyBiVjzVedC%2FU5o%3D" cctestmedia media $MEDIA_SOURCE --download --remoteresource .
+blobxfer --saskey "?sv=2015-04-05&ss=b&srt=sco&sp=rwdlac&se=2017-02-28T15:12:28Z&st=2016-11-10T07:12:28Z&spr=https&sig=RGOG%2Bj0fU%2B4q1Puwo%2B8WXq5p3%2BzDLyBiVjzVedC%2FU5o%3D" cctestmedia media $MEDIA_SOURCE --download --remoteresource --no-computefilemd5 .
 fi
 if [ "$content_environment" = 'production' ]; then
 echo $MEDIA_SOURCE --download --remoteresource .
-blobxfer --saskey "?sv=2015-04-05&ss=b&srt=sco&sp=rwdlac&se=2018-05-31T10:28:00Z&st=2016-08-27T02:28:00Z&spr=https&sig=bFWCBNiodY%2BxfcMsEMSKc2jGAB1jTiEW0yp0WqEnyVo%3D" edmedia media $MEDIA_SOURCE --download --remoteresource .
+blobxfer --saskey "?sv=2015-04-05&ss=b&srt=sco&sp=rwdlac&se=2018-05-31T10:28:00Z&st=2016-08-27T02:28:00Z&spr=https&sig=bFWCBNiodY%2BxfcMsEMSKc2jGAB1jTiEW0yp0WqEnyVo%3D" edmedia media $MEDIA_SOURCE --download --remoteresource --no-computefilemd5 .
 fi
 
 # Media Sync end
